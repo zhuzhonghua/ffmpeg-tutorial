89cee2558cc572c0301d59a8961a1b698323791c
zhonghua <zhonghua@zhonghua.com>
2020-05-16 18:46:46 +0800

    comment

--- ffmpeg_tutorial3.cpp
+++ ffmpeg_tutorial3.cpp
@@ -1,3 +1,26 @@
+// tutorial03.c
+// A pedagogical video player that will stream through every video frame as fast as it can
+// and play audio (out of sync).
+//
+// Code based on FFplay, Copyright (c) 2003 Fabrice Bellard, 
+// and a tutorial by Martin Bohme (boehme@inb.uni-luebeckREMOVETHIS.de)
+// Tested on Gentoo, CVS version 5/01/07 compiled with GCC 4.1.1
+// With updates from https://github.com/chelyaev/ffmpeg-tutorial
+// Updates tested on:
+// LAVC 54.59.100, LAVF 54.29.104, LSWS 2.1.101, SDL 1.2.15
+// on GCC 4.7.2 in Debian February 2015
+//
+// Use
+//
+// gcc -o tutorial03 tutorial03.c -lavformat -lavcodec -lswscale -lz -lm `sdl-config --cflags --libs`
+// to build (assuming libavformat and libavcodec are correctly installed, 
+// and assuming you have sdl-config. Please refer to SDL docs for your installation.)
+//
+// Run using
+// tutorial03 myvideofile.mpg
+//
+// to play the stream on your screen.
+
 #include <stdio.h>
 #include <assert.h>
 #include <SDL.h>
